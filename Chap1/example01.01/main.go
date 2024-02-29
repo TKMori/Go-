2aedfeee8794cd2758cdd8a2be89220c850d8128
TKMori <kevinjhang941201@gmail.com>
2024-02-29 16:27:40 +0800

    update: 更新程式內容與心得

--- Chap1/example01.01/main.go
+++ Chap1/example01.01/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"math/rand"
 	"time"
 )
@@ -17,6 +18,21 @@ func main() {
 	}
 
 	rand.Seed(time.Now().UnixNano())
-	index := rand.Intn(len(strs))
-	fmt.Println(strs[index])
+	index := rand.Intn(len(strs) + 5) // 這邊故意讓 index 超出範圍，讓後面有機會觸發 error
+
+	s, err := selector(strs, index)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	fmt.Println(s)
+}
+
+func selector(strs []string, index int) (string, error) {
+	if index < 0 || index >= len(strs) {
+		return "", fmt.Errorf("out of range: %d", index)
+	}
+
+	return strs[index], nil
 }
